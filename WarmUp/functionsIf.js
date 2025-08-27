9c0e6c1fc9321107ff1733131b9cba999eea3992
umangpincha <umangpincha@gmail.com>
2025-08-27 16:02:21 +0530

    Add warmup section

---
+++ WarmUp/functionsIf.js
@@ -0,0 +1,32 @@
+// function greet(name) {
+//     console.log(`Hello, ${name}!`);
+// }
+
+
+// function square(num){
+//     return num * num;
+// }
+
+// greet("Umang")
+// console.log(square(2));
+
+// function isEligibeToVote(age){
+//     age < 0 ? console.log("Invalid age") :
+//     age >= 18 ? console.log("Eligible to vote") : console.log("Not eligible to vote");
+// }
+
+// isEligibeToVote(-1);
+
+
+function isEvenOdd(num){
+    num%2 == 0 ? console.log("Even") : console.log("Odd");
+}
+
+isEvenOdd(23);
+isEvenOdd(6);
+
+
+
+
+
+
