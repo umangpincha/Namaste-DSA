7ceebf309c1fb0cd60c2bd6705bd999082ceec26
umangpincha <umangpincha@gmail.com>
2025-08-30 23:46:52 +0530

    Add loops ques

---
+++ WarmUp/loopInloop.js
@@ -0,0 +1,102 @@
+// TODO:
+// for (let i = 0; i < 4; i++) {
+//     let row = ""
+//     for (let j = 0; j < 4; j++) {
+//         row = row + "*"
+//     }
+//     console.log(row);
+// }
+
+// TODO:
+// for (let i = 1; i <= 4; i++){
+//     let row = "";
+//     for (let j = 1; j <= i; j++){
+//         row = row + "* ";
+//     }
+//     console.log(row);
+// }
+
+// TODO:
+// let n = 6;
+// for (let i = 1; i <= n; i++) {
+//     let row = ""
+//     for (let j = 1; j <= i; j++) {
+//         row = row + j;
+//     }
+//     console.log(row);
+// }
+
+// TODO:
+// let n = 5;
+// for (let i = 1; i <= 5; i++){
+//     let row = "";
+//     for (let j = 1; j <= i; j++){
+//         row += i;
+//     }
+//     console.log(row);
+// }
+
+
+// TODO:
+// let n = 5;
+// for (let i = 5; i >= 1; i--) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += j;
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+// TODO:
+let n = 6;
+// for (let i = 1; i <= n; i++){
+//     //SPACE
+//     let row="";
+//     for (let j = 1; j <= (n-i); j++){
+//         row += " ";
+//     }
+//     for (let k = 1; k <= i; k++){
+//         row+="*";
+//     }
+//     console.log(row);
+// }
+
+// TODO:
+// let n = 6;
+// for (let i = 1; i <= n; i++){
+//     let toggle = 1;
+//     let row = "";
+//     for (let j = 1; j <= i; j++){
+//         row += toggle;
+//         toggle == 1 ? toggle = 0 : toggle = 1;
+//     }
+//     console.log(row);
+// }
+
+// TODO:
+// let n = 5;
+// let toggle = 1;
+// for (let i = 1; i <= n; i++){
+//     let row = "";
+//     for (let j = 1; j <= i; j++){
+//         row += toggle;
+//         toggle == 1 ? toggle = 0 : toggle = 1;
+//     }
+//     console.log(row);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WarmUp/secondLargest.js
@@ -0,0 +1,20 @@
+// WAF to find second largest element in an array
+
+// function secondLargest(arr) {
+//     let secondLargest = -Infinity;
+//     let firstLargest = -Infinity;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > firstLargest) {
+//             secondLargest = firstLargest;
+//             firstLargest = arr[i];
+//         } else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+//             secondLargest = arr[i];
+//         }
+//     }
+//     return secondLargest;
+// }
+// let result = secondLargest([22, 34, 96, 3, 45, 2]);
+// console.log(result);
+
+
